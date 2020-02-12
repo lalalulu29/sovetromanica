1efa485424925bca36bc973a5e1825e56e158723
Kirill <k.lubarskih@mail.ru>
2020-02-12 22:56:31 +0300

    Initial Commit

---
+++ sovetromanica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sovetromanica
+//
+//  Created by Кирилл Любарских  on 12.02.2020.
+//  Copyright © 2020 Кирилл Любарских. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
